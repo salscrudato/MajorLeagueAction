1339db53b1117bca7cc81f56a8d76c3a7274be71
Salvatore Scrudato <salscrudato@Salvatores-Air.home>
2018-05-16 11:13:54 -0400

    Connected to JSON Odds and set up MLB page

--- angular-src/src/app/components/menu/menu.component.ts
+++ angular-src/src/app/components/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from '../../services/user.service';
 import {OddsService} from '../../services/odds.service';
+import {AuthService} from '../../services/auth.service';
 
 @Component({
   selector: 'app-menu',
@@ -9,24 +10,88 @@ import {OddsService} from '../../services/odds.service';
 })
 export class MenuComponent implements OnInit {
 
-  odds:any;
+  //actions: Array<Action> = [];
+  actions: Array<String> = [];
 
   constructor(
     private userService:UserService,
-    private oddsService:OddsService
-  ) {
-
-    //this.userService.getAllUsers().subscribe(data =>{
-    //  this.users = data;
-    //});
-
-    this.oddsService.getMLBOdds().subscribe(data =>{
-      this.odds = data;
-    });
-
-  }
+    private oddsService:OddsService,
+    private authService:AuthService
+  ) {}
 
   ngOnInit() {
+    this.getUserId();
+    this.oddsService.getMLBOdds().subscribe(data =>{
+    console.log(data);
+
+    for (var i = 0; i < data.length; i++) {
+      /*
+      var action = new Action(data[i].ID,
+        data[i].Details,
+        data[i].MatchTime,
+        data[i].Odds,
+        data[i].HomeTeam,
+        data[i].AwayTeam
+      );
+      */
+      //this.actions.push(action);
+      this.actions.push(data[i]);
+    }
+
+      //this.odds = data;
+    });
+  }
+
+  getUserId(){
+    this.authService.getProfile().subscribe(profile => {
+      console.log(profile.user);
+    },
+    err =>{
+      console.log(err);
+      return false;
+    });
+  }
+
+  comingSoon(){
+    alert('Feature Coming Soon!');
   }
 
 }
+/*
+class Action {
+    id: string;
+    details: string;
+    homeImagePath: string;
+    matchDate: string;
+    matchTime: string;
+    homeTeam: string;
+    awayTeam: string;
+    homeTeamML: string;
+    awayTeamML: string;
+    constructor(id: string, details: string, matchTime: string,
+      odds: any, homeTeam: string, awayTeam: string) {
+        this.id = id;
+        this.details = details;
+        this.imagePath = '/assets/images/mets.png';
+        this.matchDate = matchTime.substr(0,10);
+        this.matchTime = matchTime.substr(11,matchTime.length-3);
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        for(var i = 0; i < odds.length; i++){
+          if(odds[i].OddType=="Game"){
+            this.homeTeamML = odds[i].MoneyLineHome;
+            this.awayTeamML = odds[i].MoneyLineAway;
+          }
+        }
+        switch (this.homeTeam){
+          case "New York Mets":
+            this.homeImagePath = '/assets/images/mets.png';
+            break;
+          case "Minnesota Twins":
+            this.homeImagePath = '/assets/images/mets.png';
+            break;
+       }
+    }
+
+}
+*/
--- routes/odds.js
+++ routes/odds.js
@@ -7,20 +7,59 @@ router.get('/mlb', function(req, res, next){
   var headers = {
       'x-api-key':'72812f76-4cbe-11e8-91fa-06aae780a1ef'
   }
-
+  var actions = [];
   // Configure the request
   var options = {
       url: 'https://jsonodds.com/api/odds/mlb',
       method: 'GET',
       headers: headers
   }
-
   // Start the request
   request(options, function (error, response, body) {
       if (!error && response.statusCode == 200) {
-          res.send(body);
+          var data = JSON.parse(body);
+          for(var i = 0; i < data.length; i++){
+            var action = new Action(data[i].ID,
+              data[i].Details,
+              data[i].MatchTime,
+              data[i].Odds,
+              data[i].HomeTeam,
+              data[i].AwayTeam
+              );
+              actions.push(action);
+          }
+
+          res.send(actions);
+          //res.send(body);
       }
   });
 });
+
+class Action {
+    constructor(id, details, matchTime,
+      odds, homeTeam, awayTeam) {
+        this.id = id;
+        this.details = details;
+        this.homeImagePath = '/assets/images/mets.png';
+        this.matchDate = matchTime.substr(0,10);
+        this.matchTime = matchTime.substr(11,matchTime.length-3);
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        for(var i = 0; i < odds.length; i++){
+          if(odds[i].OddType=="Game"){
+            this.homeTeamML = odds[i].MoneyLineHome;
+            this.awayTeamML = odds[i].MoneyLineAway;
+          }
+        }
+        switch (this.homeTeam){
+          case "New York Mets":
+            this.homeImagePath = '/assets/images/mets.png';
+            break;
+          case "Minnesota Twins":
+            this.homeImagePath = '/assets/images/twins.png';
+            break;
+       }
+    }
+}
 
 module.exports = router;
