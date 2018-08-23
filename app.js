43ee2c4d5fc8c2295d548eae321f528742368459
Salvatore Scrudato <salscrudato@Salvatores-Air.home>
2018-08-22 20:54:41 -0400

    resSend

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.get('/', function(req, res){
 });
 
 app.get('*', (req,res) => {
-	res.send(__dirname + '/angular-src/src/index.html');
+	res.sendFile(path.join(__dirname, '/angular-src/src/index.html'));
 });
 
 app.listen(port, function(){
